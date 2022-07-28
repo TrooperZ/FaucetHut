e4b90e23c3bf47837a10854774cfb469db896866
TrooperZ <trooperz.tz@gmail.com>
2022-07-28 10:39:24 -0500

    Renaming

--- FaucetHut/FaucetHut/views.py
+++ FaucetHut/FaucetHut/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from BackToGenesis import app
+from FaucetHut import app
 from app import faucetinfo
 
 @app.route('/')
